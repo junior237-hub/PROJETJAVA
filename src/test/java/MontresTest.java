ab241221b62591dab536553215624cb71ddab511
Nguimene <juniorfopa@65gmail.com>
2022-02-02 13:03:16 +0100

    phase de test

---
+++ src/test/java/MontresTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MontresTest {
+
+    @Test
+    void getHeures() {
+    }
+
+    @Test
+    void getMin() {
+    }
+
+    @Test
+    void heure() {
+    }
+
+    @Test
+    void min() {
+    }
+
+    @Test
+    void avance() {
+    }
+}
---
+++ src/test/java/personnesTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class personnesTest {
+
+    @org.junit.jupiter.api.Test
+    void porter() {
+        personnes pers =new personnes("junior");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNom() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void affiche() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void retirer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void demander() {
+    }
+}
